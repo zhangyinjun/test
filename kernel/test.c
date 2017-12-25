af5f0b6a1f84dbd1804752291b7b0954c380fe77
cheryzhang <cheryzhang@126.com>
2017-12-25 10:08:08 +0800

    something new

--- kernel/test.c
+++ kernel/test.c
@@ -4,12 +4,16 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/debugfs.h>
+#include <linux/uaccess.h>
 
 static int major = 250;
 static int minor = 0;
 static dev_t devno;
 static struct class *cls;
 static struct device *test_device;
+static unsigned int a = 0;
+static struct dentry *dir = NULL, *file_a = NULL, *file_c = NULL;
 
 static int test_open(struct inode *inode, struct file *filep)
 {
@@ -20,6 +24,36 @@ static int test_open(struct inode *inode, struct file *filep)
 static struct file_operations test_ops = 
 {
 	.open = test_open,
+};
+
+static int debugfile_open(struct inode *inode, struct file *filep)
+{
+	filep->private_data = inode->i_private;
+	return 0;
+}
+
+static ssize_t debugfile_read(struct file *filep, char __user *buffer, size_t count, loff_t *ppos)
+{
+	char str[] = "welcome!\n";
+
+	count = strlen(str);
+	if (*ppos >= count)
+		return 0;
+
+	if (copy_to_user(buffer, str, count))
+	{
+		return -EFAULT;
+	}
+
+	*ppos += count;
+
+	return count;
+}
+
+static struct file_operations debugfile_ops =
+{
+	.open = debugfile_open,
+	.read = debugfile_read,
 };
 
 static int __init test_init(void)
@@ -45,6 +79,11 @@ static int __init test_init(void)
 		unregister_chrdev(major, "test");
 		return -EBUSY;
 	}
+
+	dir = debugfs_create_dir("test_dir", NULL);
+	file_a = debugfs_create_u32("test_a", 0644, dir, &a);
+	file_c = debugfs_create_file("test_c", 0444, dir, NULL, &debugfile_ops);
+
 	printk(KERN_ALERT "test_init\n");
 	
 	return 0;
@@ -55,7 +94,8 @@ static void test_exit(void)
 	device_destroy(cls, devno);
 	class_destroy(cls);
 	unregister_chrdev(major, "test");
-	printk(KERN_ALERT "test_exit\n");
+	debugfs_remove_recursive(dir);
+	printk(KERN_ALERT "test_exit[%u]\n", a);
 }
 
 MODULE_AUTHOR("zhangyinjun");
--- test.c
+++ test.c
@@ -2,10 +2,16 @@
 #include "stdio.h"
 #include "string.h"
 #include "typedef.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 int testcase1(void)
 {
-    ST_TEST st0, st1;
+    ST_TEST st0, st1, st2={1234,12,34,"abc"};
     char str[] = {'T', 'h', 'i', 's'};
     
     st0.a = 100;
@@ -17,6 +23,7 @@ int testcase1(void)
 
     printf("st0:{%d,%d,%s}\n", st0.a, st0.b, st0.p);
     printf("st1:{%d,%d,%s}\n", st1.a, st1.b, st1.p);
+    printf("st2:{%d,%d,%s}\n", st2.a, st2.b, st2.p);
     printf("size:%d\n", sizeof(ST_TEST));
     return 0;
 }
@@ -34,7 +41,7 @@ int testcase2(void)
 
 int testcase3(void)
 {
-    printf("sizeof pointer: %u\r\n", sizeof(char *));
+    printf("sizeof pointer: %u, sizeof long: %u, size of ull: %u\r\n", sizeof(char *), sizeof(unsigned long), sizeof(unsigned long long));
     return 0;
 }
 
@@ -59,7 +66,114 @@ int testcase5(void)
     return 0;
 }
 
+#define BUFFER_SIZE	128
+#define PORT		8888
+void handler(int signo)
+{
+    pid_t pid;
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0)
+    {
+        printf("child(%d) is over!\r\n", pid);
+    }
+}
+
+int testcase6(void)
+{
+    int listenfd, clientfd;
+    int n;
+    pid_t pid;
+    struct sockaddr_in serveraddr, clientaddr;
+    socklen_t peerlen;
+    char buffer[BUFFER_SIZE];
+
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("create socket fail.");
+        return -1;
+    }
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(listenfd, (struct sockaddr *)(&serveraddr), sizeof(serveraddr)) < 0)
+    {
+        perror("bind fail.");
+        close(listenfd);
+        return -1;
+    }
+
+    if (listen(listenfd, 10) == -1)
+    {
+        perror("listen fail.");
+        close(listenfd);
+        return -1;
+    }
+
+    signal(SIGCHLD, handler);
+    peerlen = sizeof(clientaddr);
+    while (1)
+    {
+        if ((clientfd = accept(listenfd, (struct sockaddr *)(&clientaddr), &peerlen)) < 0)
+        {
+            perror("accept fail.");
+            close(listenfd);
+            return -1;
+        }
+        else
+        {
+            printf("connect from %s:%d\r\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+        }
+
+        if ((pid = fork()) < 0)
+        {
+            perror("fork fail.");
+            close(listenfd);
+            close(clientfd);
+            return -1;
+        }
+        else if (0 == pid)
+        {
+            close(listenfd);
+            while (1)
+            {
+                if ((n = recv(clientfd, buffer, BUFFER_SIZE, 0)) < 0)
+                {
+                    perror("recv fail.");
+                    close(clientfd);
+                    exit(-1);
+                }
+                else if (n == 0)
+                    break;
+                else
+                {
+                    buffer[n] = '\0';
+                    printf("recv mesg: %s[%d]\n", buffer, n);
+                }
+                
+                if (send(clientfd, buffer, n, 0) < 0)
+                {
+                    perror("send fail.");
+                    close(clientfd);
+                    exit(-1);
+                }
+            }
+            close(clientfd);
+            printf("client closed.\r\n");
+            exit(0);
+        }
+        else
+        {
+            close(clientfd);
+        }
+    }
+
+    close(listenfd);
+    return 0;
+}
+
 int main(int argc, const char *argv[])
 {
-    return testcase5();
+    return testcase3();
 }
