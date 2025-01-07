0d4ea63dbcaf3b0a66e7795dfa826388cf6f8c38
Yinjun Zhang <zhangyinjun@sidynamic.com>
2025-01-07 09:04:36 +0000

    kernel: add a test case of netdev/switchdev nb
    Signed-off-by: Yinjun Zhang <zhangyinjun@sidynamic.com>

--- kernel/test.c
+++ kernel/test.c
@@ -6,14 +6,19 @@
 #include <linux/device.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
+#include <linux/etherdevice.h>
+#include <net/switchdev.h>
 
+#define TEST_CASE1
+
+#ifdef TEST_CASE0
 static int major = 250;
 static int minor = 0;
 static dev_t devno;
 static struct class *cls;
 static struct device *test_device;
 static unsigned int a = 0;
-static struct dentry *dir = NULL, *file_a = NULL, *file_c = NULL;
+static struct dentry *dir = NULL;
 
 static int test_open(struct inode *inode, struct file *filep)
 {
@@ -56,14 +61,17 @@ static struct file_operations debugfile_ops =
 	.read = debugfile_read,
 };
 
-static int __init test_init(void)
+static int test_init_case0(void)
 {
+	u8 mac[6];
 	int ret;
 
+	eth_random_addr(mac);
+	printk("mac: [%pM]\n", mac);
 	devno = MKDEV(major, minor);
 	ret = register_chrdev(major, "test", &test_ops);
 	if (ret)
-		return -1;
+		return ret;
 
 	cls = class_create(THIS_MODULE, "test_class");
 	if (IS_ERR(cls))
@@ -81,21 +89,114 @@ static int __init test_init(void)
 	}
 
 	dir = debugfs_create_dir("test_dir", NULL);
-	file_a = debugfs_create_u32("test_a", 0644, dir, &a);
-	file_c = debugfs_create_file("test_c", 0444, dir, NULL, &debugfile_ops);
-
-	printk(KERN_ALERT "test_init\n");
+	debugfs_create_u32("test_a", 0644, dir, &a);
+	debugfs_create_file("test_c", 0444, dir, NULL, &debugfile_ops);
 	
+	printk(KERN_ALERT "test_init_case0\n");
+
 	return 0;
 }
 
-static void test_exit(void)
+static void test_exit_case0(void)
 {
 	device_destroy(cls, devno);
 	class_destroy(cls);
 	unregister_chrdev(major, "test");
 	debugfs_remove_recursive(dir);
-	printk(KERN_ALERT "test_exit[%u]\n", a);
+	printk(KERN_ALERT "test_exit_case0 [%u]\n", a);
+}
+#endif
+
+#ifdef TEST_CASE1
+
+static int test_netdev_event(struct notifier_block *unused,
+			     unsigned long event, void *ptr)
+{
+	struct net_device *dev = netdev_notifier_info_to_dev(ptr);
+
+	printk("netdev event %lu from dev %s\n", event, dev->name);
+	return NOTIFY_DONE;
+}
+
+static int test_switchdev_event(struct notifier_block *unused,
+				unsigned long event, void *ptr)
+{
+	struct net_device *dev = switchdev_notifier_info_to_dev(ptr);
+
+	printk("switchdev event %lu from dev %s\n", event, dev->name);
+	return NOTIFY_DONE;
+}
+
+static int test_switchdev_blk_event(struct notifier_block *unused,
+				    unsigned long event, void *ptr)
+{
+	struct net_device *dev = switchdev_notifier_info_to_dev(ptr);
+
+	printk("switchdev blk event %lu from dev %s\n", event, dev->name);
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block test_netdev_nb = {
+	.notifier_call = test_netdev_event,
+};
+
+static struct notifier_block test_switchdev_nb = {
+	.notifier_call = test_switchdev_event,
+};
+
+static struct notifier_block test_switchdev_blk = {
+	.notifier_call = test_switchdev_blk_event,
+};
+
+static int test_init_case1(void)
+{
+	int ret = register_netdevice_notifier(&test_netdev_nb);
+	if (ret)
+		return ret;
+
+	ret = register_switchdev_notifier(&test_switchdev_nb);
+	if (ret) {
+		unregister_netdevice_notifier(&test_netdev_nb);
+		return ret;
+	}
+
+	ret = register_switchdev_blocking_notifier(&test_switchdev_blk);
+	if (ret) {
+		unregister_switchdev_notifier(&test_switchdev_nb);
+		unregister_netdevice_notifier(&test_netdev_nb);
+		return ret;
+	}
+
+	return 0;
+}
+
+static void test_exit_case1(void)
+{
+	unregister_switchdev_blocking_notifier(&test_switchdev_blk);
+	unregister_switchdev_notifier(&test_switchdev_nb);
+	unregister_netdevice_notifier(&test_netdev_nb);
+}
+#endif
+
+static int __init test_init(void)
+{
+#ifdef TEST_CASE0
+	return test_init_case0();
+#endif
+#ifdef TEST_CASE1
+	return test_init_case1();
+#endif
+	return 0;
+}
+
+static void test_exit(void)
+{
+#ifdef TEST_CASE0
+	test_exit_case0();
+#endif
+#ifdef TEST_CASE1
+	test_exit_case1();
+#endif
 }
 
 MODULE_AUTHOR("zhangyinjun");
